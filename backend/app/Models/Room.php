705e5ed89220b2a331476aa81b4503a567e109af
Barakael Lucas <barakaellucas2019@gmail.com>
2025-12-05 16:31:33 +0300

    Add Room and Shelf models
    - Introduced Room and Shelf models to represent room and shelf entities in the application.
    - Defined fillable attributes for both models to facilitate mass assignment.
    - Established relationships between Room and Shelf, as well as between Shelf and Panel, to support data integrity and retrieval.

---
+++ backend/app/Models/Room.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Room extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'description',
+    ];
+
+    public function shelves()
+    {
+        return $this->hasMany(Shelf::class);
+    }
+
+    public function panels()
+    {
+        return $this->hasMany(Panel::class);
+    }
+
+    public function users()
+    {
+        return $this->belongsToMany(User::class, 'user_rooms');
+    }
+}
+
