b7f52ecd0ead3bca666e7de2ccaea963e9d88f4d
barakael <barakaellucas2019@gmail.com>
2026-02-24 12:08:53 +0300

    feat(factory): create RoomFactory for generating Room model instances

---
+++ backend/database/factories/RoomFactory.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Room;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends Factory<Room>
+ */
+class RoomFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'name' => $this->faker->word() . ' Room',
+            'description' => $this->faker->sentence(),
+            'subscription_status' => 'no_subscription',
+        ];
+    }
+}
